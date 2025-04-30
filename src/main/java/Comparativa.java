6b6a894400f7c1c7bfe68c782c87daa012473406
CPL <carlos@centropyme.com>
2025-04-30 09:59:47 +0200

    * Creado pool de conexiones y configurado el tamaño máximo. * Pruebas con 1, 10, 100 y 300 conexiones

--- src/main/java/Comparativa.java
+++ src/main/java/Comparativa.java
@@ -1,3 +1,6 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,14 +9,15 @@ public class Comparativa {
     private static final String URL="jdbc:postgresql://localhost:5432/jardineria";
     private static final String USUARIO= "jardinero";
     private static final String CLAVE="jardinero";
-    private static final int NUMCONEX = 100;
+    private static final int NUMCONEX = 300;
 
     /**
      * Comparativa
      * Conexiones       DM          Pool (Hikari)
-     *      1           884
-     *     10         1.890
-     *    100         8.747
+     *      1           800             316
+     *     10         1.653             220
+     *    100         7.909             159
+     *    300        22.221             162
      */
 
     public static void main(String[] args) throws SQLException {
@@ -24,5 +28,24 @@ public class Comparativa {
             con.close();
         }
         System.out.println("Tiempo Total DM: "+(System.currentTimeMillis()-inicio));
+
+        // Probamos con un pool de conexiones
+        // Configuramos el pool
+        HikariConfig conf = new HikariConfig();
+        conf.setJdbcUrl(URL);
+        conf.setUsername(USUARIO);
+        conf.setPassword(CLAVE);
+        conf.setMaximumPoolSize(NUMCONEX);
+
+        inicio = System.currentTimeMillis();
+        HikariDataSource hkds = new HikariDataSource(conf);
+        for(int i = 0; i < NUMCONEX; i++){
+            Connection con = hkds.getConnection();
+
+            con.close();
+        }
+        hkds.close();
+        System.out.println("Tiempo Total Pool: "+(System.currentTimeMillis()-inicio));
+
     }
 }
